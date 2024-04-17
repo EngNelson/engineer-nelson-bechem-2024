a5840372bdb792289c91fac8ba26ec7e4fcf35aa
EngNelson <bechem200@gmail.com>
2024-04-17 14:29:09 +0100

    about section in progress

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './about.css'
 import ME from '../../assets/about-me.jpg'
 import { FaAward } from "react-icons/fa6";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
   return (
@@ -15,24 +17,34 @@ const About = () => {
                <img src={ME} alt="About Image" />
            </div>
       </div>
+
        <div className="about__content">
          <div className="about__cards">
+
             <article className='about__card'>
             <FaAward className='about__icon'/>
-               <h5>Experience</h5>
+                   <h5>Experience</h5>
                    <small>5+ Years Working</small>
             </article>
+
             <article className='about__card'>
-            <FaAward className='about__icon'/>
-               <h5>Experience</h5>
-                   <small>5+ Years Working</small>
+            <FiUsers className='about__icon'/>
+               <h5>Clients</h5>
+                   <small>200+ Worldwide</small>
             </article>
+
             <article className='about__card'>
-            <FaAward className='about__icon'/>
-               <h5>Experience</h5>
-                   <small>5+ Years Working</small>
+            <VscFolderLibrary className='about__icon'/>
+               <h5>Projects</h5>
+                   <small>80+ Completed</small>
             </article>
          </div>
+           
+           <p>
+           Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, 
+           no sea takimata sanctus est Lorem ipsum dolor sit amet.
+           </p>
+         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
        </div>
     </div>
     </section>
