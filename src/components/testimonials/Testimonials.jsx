9a80e2ce74feca98361db4a4268c364eabde7f9b
EngNelson <bechem200@gmail.com>
2024-04-19 12:02:46 +0100

    testimonial module

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -3,7 +3,25 @@ import './testimonials.css'
 
 const Testimonials = () => {
   return (
-    <section id="testimonials">Testimonials</section>
+    <section id="testimonials">
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+         <article className='testimonial'>
+            <div className="client__avatar">
+              <img src="" alt="Avatar" />
+            </div>
+            <h5 className='client__name'>Engineer Kambang</h5>
+              <small className='client__review'>
+                Lorem isopf dolor sit contsection adipisicing elit.
+                Lorem isopf dolor sit contsection adipisicing elit.
+                Lorem isopf dolor sit contsection adipisicing elit.
+              </small>
+         </article>
+      </div>
+    
+    </section>
   )
 }
 
