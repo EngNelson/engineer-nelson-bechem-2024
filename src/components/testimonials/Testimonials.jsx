0ee25ccb32e342dbbfe27a91edc8816333e32d31
EngNelson <bechem200@gmail.com>
2024-04-19 14:36:44 +0100

    testimonial module

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,6 +1,35 @@
 import React from 'react'
 import './testimonials.css'
+import Avt1 from '../../assets/Avt1.jpg'
 
+
+const data = [
+  {
+    avatar: Avt1,
+    name: 'Engineer Kambang',
+    review: 
+    ' Lorem isopf dolor sit contsection adipisicingelitLorem isopf dolor sit contsection adipisicing elit'
+  },
+  {
+    avatar: Avt1,
+    name: 'Engineer Kambang',
+    review: 
+    ' Lorem isopf dolor sit contsection adipisicingelitLorem isopf dolor sit contsection adipisicing elit'
+  },
+  {
+    avatar: Avt1,
+    name: 'Engineer Kambang',
+    review: 
+    ' Lorem isopf dolor sit contsection adipisicingelitLorem isopf dolor sit contsection adipisicing elit'
+  },
+  {
+    avatar: Avt1,
+    name: 'Engineer Kambang',
+    review: 
+    ' Lorem isopf dolor sit contsection adipisicingelitLorem isopf dolor sit contsection adipisicing elit'
+  },
+
+]
 const Testimonials = () => {
   return (
     <section id="testimonials">
@@ -8,17 +37,19 @@ const Testimonials = () => {
       <h2>Testimonials</h2>
 
       <div className="container testimonials__container">
-         <article className='testimonial'>
-            <div className="client__avatar">
-              <img src="" alt="Avatar" />
-            </div>
-            <h5 className='client__name'>Engineer Kambang</h5>
-              <small className='client__review'>
-                Lorem isopf dolor sit contsection adipisicing elit.
-                Lorem isopf dolor sit contsection adipisicing elit.
-                Lorem isopf dolor sit contsection adipisicing elit.
-              </small>
-         </article>
+         {
+          data.map(({avatar, name, review}, index) => {
+             return(
+              <article key={index} className='testimonial'>
+              <div className="client__avatar">
+                <img src={Avt1} />
+              </div>
+              <h5 className='client__name'>{name}</h5>
+                <small className='client__review'>{review}</small>
+           </article>
+             )
+          })
+         }
       </div>
     
     </section>
