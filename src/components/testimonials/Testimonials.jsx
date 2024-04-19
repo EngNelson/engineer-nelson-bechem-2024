7290081854e21543354fc7628d9e193d3ed97818
EngNelson <bechem200@gmail.com>
2024-04-19 15:29:02 +0100

    testimonial module

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import './testimonials.css'
 import Avt1 from '../../assets/Avt1.jpg'
+// import Swiper core and required modules
+import { Pagination } from 'swiper/modules';
+import {Swiper, SwiperSlide } from 'swiper/react';
+
+
+// Import Swiper Styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 
 
 const data = [
@@ -36,21 +45,29 @@ const Testimonials = () => {
       <h5>Review from clients</h5>
       <h2>Testimonials</h2>
 
-      <div className="container testimonials__container">
+      <Swiper className="container testimonials__container"
+      // Swiper Modules
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      Pagination={{clickable: true}}
+      >
+
+
          {
           data.map(({avatar, name, review}, index) => {
              return(
-              <article key={index} className='testimonial'>
+              <SwiperSlide key={index} className='testimonial'>
               <div className="client__avatar">
                 <img src={Avt1} />
               </div>
               <h5 className='client__name'>{name}</h5>
                 <small className='client__review'>{review}</small>
-           </article>
+           </SwiperSlide>
              )
           })
          }
-      </div>
+      </Swiper>
     
     </section>
   )
