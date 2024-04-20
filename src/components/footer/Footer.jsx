8d9d314d6066b306efd81fe83d58b254807e2593
EngNelson <bechem200@gmail.com>
2024-04-20 11:56:28 +0100

    footer module

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,6 +8,7 @@ const Footer = () => {
   return (
     <footer>
       <a href="#" className='footer__logo'>NELSON</a>
+
       <ul className='permalinks'>
         <li><a href="#">Home</a></li>
         <li><a href="#about">About</a></li>
@@ -16,13 +17,12 @@ const Footer = () => {
         <li><a href="#portfolio">Portfolio</a></li>
         <li><a href="#testimonials">Testimonials</a></li>
         <li><a href="#contact">Contact</a></li>
-
       </ul>
+
         <div className="footer__socials">
           <a href="https://facebook.com"><FaFacebook/></a>
           <a href="https://instagram.com"><FiInstagram/></a>
           <a href="https://twitter.com"><IoLogoTwitter/></a>
-        
         </div>
 
         <div className="footer__copyright">
